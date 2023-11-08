026a1e9fbdd3648f157863bf55aef07229aa4ae0
Tomasz Pietrek <tomasz@nats.io>
2023-11-08 13:11:11 +0100

    Add compatibility go keys test
    Signed-off-by: Tomasz Pietrek <tomasz@nats.io>

--- Tests/nkeys.swiftTests/nkeys_swiftTests.swift
+++ Tests/nkeys.swiftTests/nkeys_swiftTests.swift
@@ -22,4 +22,16 @@ final class nkeys_swiftTests: XCTestCase {
         let signature = try pair.sign(input: message)
         try pair.verify(input: message, signature: signature)
     }
+    
+    func test_roundtrip_encoding_go_compatibility() throws {
+        
+        let seed = "SAAPN4W3EG6KCJGUQTKTJ5GSB5NHK5CHAJL4DBGFUM3HHROI4XUEP4OBK4";
+        let pk = "ACODERUVFFAWZQDSS6SBIACUA5O6SXF7HJ3YTYXBALHZP3P7R4BUO4J2";
+        
+        let pair = try KeyPair(seed: seed)
+        
+        let swift_seed = try pair.seed
+        XCTAssert(seed == swift_seed)
+        XCTAssert(pk == pair.publicKeyEncoded)
+    }
 }
