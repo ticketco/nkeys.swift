6e929624e73be08a6fefbea66bb3dab84c3b4b29
Tomasz Pietrek <tomasz@nats.io>
2023-11-08 13:03:14 +0100

    Add tests
    Signed-off-by: Tomasz Pietrek <tomasz@nats.io>

--- Tests/nkeys.swiftTests/nkeys_swiftTests.swift
+++ Tests/nkeys.swiftTests/nkeys_swiftTests.swift
@@ -2,11 +2,24 @@ import XCTest
 @testable import nkeys_swift
 
 final class nkeys_swiftTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    
+    func test_seedEncodeDecodeRoundtrip() throws {
+        let pair = try KeyPair.init(keyPairType: KeyPairType.user)
+        let seed = try pair.seed
+        let pub = pair.publicKeyEncoded
+        
+        let pair2 = try KeyPair.init(seed: seed)
+        let seed2 = try pair2.seed
+        
+        XCTAssert(seed == seed2)
+        XCTAssert(pub == pair2.publicKeyEncoded)
+    }
+    
+    func test_signAndVerify() throws {
+        let pair = try KeyPair(keyPairType: KeyPairType.user)
+        let message = "A very secret message".data(using: .utf8)!
+        
+        let signature = try pair.sign(input: message)
+        try pair.verify(input: message, signature: signature)
     }
 }
